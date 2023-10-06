a34be0803ae544ab568a3d41c5c284b3e7037ec7
Kaan Pasa <45922962+kaanpasa@users.noreply.github.com>
2023-10-06 13:41:32 +0300

    Create function-composition.js

---
+++ function-composition.js
@@ -0,0 +1,10 @@
+var compose = function(functions) {
+  if (functions.length === 0) {
+    return function(x) { return x; };
+  }
+  return functions.reduceRight(function(prevFn, nextFn) {
+    return function(x) {
+      return nextFn(prevFn(x));
+    };
+  });
+};
